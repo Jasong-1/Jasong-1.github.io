e21030cad3858271c8d79656d2e2207cef6534b1
jgeng@rtresponse.com <gzjxigongda@gmail.com>
2022-03-25 18:26:07 +0800

    032522.1: Upgrade for Adaptive Display - Overall

--- js/index.js
+++ js/index.js
@@ -1,25 +1,29 @@
-$("#solution_nav").on("click", function () {
-    $("html,body").animate({ scrollTop: $("#solution").offset().top }, 1000);
+$("#solution_nav").on("click", function() {
+	$("html,body").animate({
+		scrollTop: $("#solution").offset().top
+	}, 1000);
 });
 
 
 // Index Dialog
 
-$(".modal-trigger").on("click", function () {
-    var url = $(this).attr("url")
-    $("#requestDemoDialog").find(".skip-link").attr("href", url)
-    $("#requestDemoDialog").modal({
-        showClose: false
-    });
+$(".hamburger-menu i").on("click", function() {
+	const flag = $(".nav-wrap")[0].style.display;
+	console.log(flag);
+	flag === "block" ? $(".nav-wrap").hide() : $(".nav-wrap").show();
 })
-$(".dialog-request-demo").on("click", function () {
-    $.modal.close();
+
+$(".modal-trigger").on("click", function() {
+	var url = $(this).attr("url")
+	$("#requestDemoDialog").find(".skip-link").attr("href", url)
+	$("#requestDemoDialog").modal({
+		showClose: false
+	});
 })
-$(".hamburger-menu i").on("click", function () {
-    const flag = $(".nav")[0].style.display;
-    console.log(flag);
-    flag === "block" ? $(".nav").hide() : $(".nav").show();
+$(".dialog-request-demo").on("click", function() {
+	$.modal.close();
 })
+
 
 
 
@@ -27,16 +31,17 @@ $(".hamburger-menu i").on("click", function () {
 
 // get current year
 
-function getcurrentyear(){
-    var now=new Date();
-    var year=now.getFullYear();
-    time=year;
-    var currentyear=document.getElementById('currentyear');
-    currentyear.innerHTML=time;
+function getcurrentyear() {
+	var now = new Date();
+	var year = now.getFullYear();
+	time = year;
+	var currentyear = document.getElementById('currentyear');
+	currentyear.innerHTML = time;
 }
-function letstart(){
-    taskld=setInterval(getcurrentyear,500);
-    }
-    window.onload=function(){
-    letstart();
+
+function letstart() {
+	taskld = setInterval(getcurrentyear, 500);
+}
+window.onload = function() {
+	letstart();
 }
