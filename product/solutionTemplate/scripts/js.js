b956ca5109c06ce3a95e59b93a36196703f41171
jgeng@rtresponse.com <gzjxigongda@gmail.com>
2023-06-02 22:16:03 +0800

    06022023.1

---
+++ product/solutionTemplate/scripts/js.js
@@ -0,0 +1,12 @@
+function AppLevelBack() {
+    window.location.href="../AppListNav.html"
+}
+function EventSubLevelBack() {
+    window.location.href="../SolutionType/Event.html"
+}
+function TimeConfirmSubLevelBack() {
+    window.location.href="../../../TimeConfirm.html"
+}
+function FormSolutionSubLevelBack() {
+    window.location.href="../SolutionType/FormSolution.html"
+}
