5f0eee51ecd6ebdf6229e8d54a198885c7211a4b
Jasong-1 <gzjxigongda@gmail.com>
2023-06-06 22:03:00 +0800

    06062023.1

--- product/solutionTemplate/scripts/js.js
+++ product/solutionTemplate/scripts/js.js
@@ -1,12 +1,27 @@
 function AppLevelBack() {
-    window.location.href="../AppListNav.html"
+	window.location.href = "../AppListNav.html";
 }
+
 function EventSubLevelBack() {
-    window.location.href="../SolutionType/Event.html"
+	window.location.href = "../SolutionType/Event.html";
 }
+
 function TimeConfirmSubLevelBack() {
-    window.location.href="../../../TimeConfirm.html"
+	window.location.href = "../../../TimeConfirm.html";
 }
+
 function FormSolutionSubLevelBack() {
-    window.location.href="../SolutionType/FormSolution.html"
+	window.location.href = "../SolutionType/FormSolution.html";
+}
+
+function switchweddingSetting() {
+	window.location.href = "../../../../SolutionType/Event/general User/Wedding/setting1.html";
+}
+
+function switchConferenceSetting() {
+	window.location.href = "../../../../SolutionType/TimeConfirm/general User/Parent-Teacher Conference/setting1.html";
+}
+
+function switchSportSetting() {
+	window.location.href = "../../../../SolutionType/FormSolution/general User/StudentReg/setting1.html";
 }
