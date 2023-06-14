68b825ecdd39561d12159e3beb393276bcc5a525
Jasong-1 <gzjxigongda@gmail.com>
2023-06-14 19:23:17 +0800

    location and link update

--- product/solutionTemplate/scripts/js.js
+++ product/solutionTemplate/scripts/js.js
@@ -14,28 +14,91 @@ function FormSolutionSubLevelBack() {
 	window.location.href = "../SolutionType/FormSolution.html";
 }
 
+
+
+// Event Planning
+
 function switchweddingSetting() {
 	window.location.href = "../../../../SolutionType/Event/general User/Wedding/setting1.html";
 }
-
-function switchConferenceSetting() {
-	window.location.href = "../../../../SolutionType/TimeConfirm/general User/Parent-Teacher Conference/setting1.html";
-}
-
-function switchSportSetting() {
-	window.location.href = "../../../../SolutionType/FormSolution/general User/StudentReg/setting1.html";
-}
-
-function switchEventCoordinationSetting() {
-	window.location.href = "../../../../SolutionType/ScheduleCoordintion/general User/MeetingCoordination/setting1.html";
-}
-
 function switchSGamesSetting() {
 	window.location.href = "../../../../SolutionType/Event/general User/SportGames/setting1.html";
+}
+function switchConferenceSetting() {
+	window.location.href = "../../../../SolutionType/Event/general User/Conference/setting1.html";
+}
+function switchBabySittingSetting() {
+	window.location.href = "../../../../SolutionType/Event/general User/BabySitting/setting1.html";
+}
+function switchPerformanceReviewSetting() {
+	window.location.href = "../../../../SolutionType/Event/general User/PerformanceReview/setting1.html";
+}
+function switchGovernmentAgencySetting() {
+	window.location.href = "../../../../SolutionType/Event/general User/GovernmentAgency/setting1.html";
+}
+function switchBabyShowerSetting() {
+	window.location.href = "../../../../SolutionType/Event/general User/BabyShower/setting1.html";
+}
+
+
+
+
+// Calendar Booking
+
+function switchPTeacherConferenceSetting() {
+	window.location.href = "../../../../SolutionType/TimeConfirm/general User/Parent-Teacher Conference/setting1.html";
 }
 function switchClientsMeetingSetting() {
 	window.location.href = "../../../../SolutionType/TimeConfirm/general User/ClientsMeeting/setting1.html";
 }
+function switchInterviewsSetting() {
+	window.location.href = "../../../../SolutionType/TimeConfirm/general User/Interviews/setting1.html";
+}
+function switchMedicalPSetting() {
+	window.location.href = "../../../../SolutionType/TimeConfirm/general User/MedicalAndPatient/setting1.html";
+}
+function switchStoreAppointmentSetting() {
+	window.location.href = "../../../../SolutionType/TimeConfirm/general User/StoreAppointment/setting1.html";
+}
+function switchPersonalTrainerSetting() {
+	window.location.href = "../../../../SolutionType/TimeConfirm/general User/PersonalTrainer/setting1.html";
+}
+function switchRealEstateSetting() {
+	window.location.href = "../../../../SolutionType/TimeConfirm/general User/RealEstate/setting1.html";
+}
+function switchTutoringSchedulingSetting() {
+	window.location.href = "../../../../SolutionType/TimeConfirm/general User/TutoringScheduling/setting1.html";
+}
+function switchAcademicAdvisorsSetting() {
+	window.location.href = "../../../../SolutionType/TimeConfirm/general User/AcademicAdvisors/setting1.html";
+}
+
+
+
+
+// Schedule Coordination
+
+function switchEventCoordinationSetting() {
+	window.location.href = "../../../../SolutionType/ScheduleCoordintion/general User/MeetingCoordination/setting1.html";
+}
 function switchFamilyReunionSetting() {
 	window.location.href = "../../../../SolutionType/ScheduleCoordintion/general User/FamilyReunion/setting1.html";
+}
+function switchGroupStudySessionSetting() {
+	window.location.href = "../../../../SolutionType/ScheduleCoordintion/general User/GroupStudySession/setting1.html";
+}
+function switchTeamBuildingEventSetting() {
+	window.location.href = "../../../../SolutionType/ScheduleCoordintion/general User/TeamBuildingEvent/setting1.html";
+}
+function switchGroupProjectMeetingSetting() {
+	window.location.href = "../../../../SolutionType/ScheduleCoordintion/general User/GroupProjectMeeting/setting1.html";
+}
+function switchVacationPlanningSetting() {
+	window.location.href = "../../../../SolutionType/ScheduleCoordintion/general User/VacationPlanning/setting1.html";
+}
+function switchTripPlanningSetting() {
+	window.location.href = "../../../../SolutionType/ScheduleCoordintion/general User/TripPlanning/setting1.html";
+}
+function switchSocialEventSetting() {
+	window.location.href = "../../../../SolutionType/ScheduleCoordintion/general User/SocialEvent/setting1.html";
 }
--- product/solutionTemplate/scripts/map.js
+++ product/solutionTemplate/scripts/map.js
@@ -1,32 +1,100 @@
-/**
- * @license
- * Copyright 2019 Google LLC. All Rights Reserved.
- * SPDX-License-Identifier: Apache-2.0
- */
-// Initialize and add the map
-let map;
+"use strict";
 
-async function initMap() {
-  // The location of Uluru
-  const position = { lat: -25.344, lng: 131.031 };
-  // Request needed libraries.
-  //@ts-ignore
-  const { Map } = await google.maps.importLibrary("maps");
-  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+			function initMap() {
+				const CONFIGURATION = {
+					"ctaTitle": "Checkout",
+					"mapOptions": {
+						"center": {
+							"lat": 37.4221,
+							"lng": -122.0841
+						},
+						"fullscreenControl": true,
+						"mapTypeControl": false,
+						"streetViewControl": true,
+						"zoom": 11,
+						"zoomControl": true,
+						"maxZoom": 22,
+						"mapId": ""
+					},
+					"mapsApiKey": "AIzaSyCH5DC2pvTEVVRytSxpcLixjDRw4Yp6mFE",
+					"capabilities": {
+						"addressAutocompleteControl": true,
+						"mapDisplayControl": true,
+						"ctaControl": true
+					}
+				};
+				const componentForm = [
+					'location',
+					'locality',
+					'administrative_area_level_1',
+					'country',
+					'postal_code',
+				];
 
-  // The map, centered at Uluru
-  map = new Map(document.getElementById("map"), {
-    zoom: 4,
-    center: position,
-    mapId: "DEMO_MAP_ID",
-  });
+				const getFormInputElement = (component) => document.getElementById(component + '-input');
+				const map = new google.maps.Map(document.getElementById("gmp-map"), {
+					zoom: CONFIGURATION.mapOptions.zoom,
+					center: {
+						lat: 37.4221,
+						lng: -122.0841
+					},
+					mapTypeControl: false,
+					fullscreenControl: CONFIGURATION.mapOptions.fullscreenControl,
+					zoomControl: CONFIGURATION.mapOptions.zoomControl,
+					streetViewControl: CONFIGURATION.mapOptions.streetViewControl
+				});
+				const marker = new google.maps.Marker({
+					map: map,
+					draggable: false
+				});
+				const autocompleteInput = getFormInputElement('location');
+				const autocomplete = new google.maps.places.Autocomplete(autocompleteInput, {
+					fields: ["address_components", "geometry", "name"],
+					types: ["address"],
+				});
+				autocomplete.addListener('place_changed', function() {
+					marker.setVisible(false);
+					const place = autocomplete.getPlace();
+					if (!place.geometry) {
+						// User entered the name of a Place that was not suggested and
+						// pressed the Enter key, or the Place Details request failed.
+						window.alert('No details available for input: \'' + place.name + '\'');
+						return;
+					}
+					renderAddress(place);
+					fillInAddress(place);
+				});
 
-  // The marker, positioned at Uluru
-  const marker = new AdvancedMarkerElement({
-    map: map,
-    position: position,
-    title: "Uluru",
-  });
-}
+				function fillInAddress(place) { // optional parameter
+					const addressNameFormat = {
+						'street_number': 'short_name',
+						'route': 'long_name',
+						'locality': 'long_name',
+						'administrative_area_level_1': 'short_name',
+						'country': 'long_name',
+						'postal_code': 'short_name',
+					};
+					const getAddressComp = function(type) {
+						for (const component of place.address_components) {
+							if (component.types[0] === type) {
+								return component[addressNameFormat[type]];
+							}
+						}
+						return '';
+					};
+					getFormInputElement('location').value = getAddressComp('street_number') + ' ' +
+						getAddressComp('route');
+					for (const component of componentForm) {
+						// Location field is handled separately above as it has different logic.
+						if (component !== 'location') {
+							getFormInputElement(component).value = getAddressComp(component);
+						}
+					}
+				}
 
-initMap();
+				function renderAddress(place) {
+					map.setCenter(place.geometry.location);
+					marker.setPosition(place.geometry.location);
+					marker.setVisible(true);
+				}
+			}
