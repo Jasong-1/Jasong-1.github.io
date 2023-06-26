2a81e00476e2bb3bcc2ccde0470c7a3cacce7f20
gzjxigongda@gmail.com <gzjxigongda@gmail.com>
2023-06-26 21:21:07 +0800

    06262023.1 New Version

--- product/solutionTemplate/scripts/js.js
+++ product/solutionTemplate/scripts/js.js
@@ -1,3 +1,179 @@
+// Personal
+
+function switchToPBabyShower() {
+	window.location.href = "../../../Personal/TemplateA/BabyShower/setting1.html";
+}
+function switchToPBabysitting() {
+	window.location.href = "../../../Personal/TemplateA/Babysitting/setting1.html";
+}
+function switchToPGovernmentAgency() {
+	window.location.href = "../../../Personal/TemplateA/GovernmentAgency/setting1.html";
+}
+function switchToPSportGames() {
+	window.location.href = "../../../Personal/TemplateA/SportGames/setting1.html";
+}
+function switchToPWedding() {
+	window.location.href = "../../../Personal/TemplateA/Wedding/setting1.html";
+}
+function switchToPClientsMeeting() {
+	window.location.href = "../../../Personal/TemplateB/ClientsMeeting/setting1.html";
+}
+function switchToPInterviews() {
+	window.location.href = "../../../Personal/TemplateB/Interviews/setting1.html";
+}
+function switchToPMedicalAndPatient() {
+	window.location.href = "../../../Personal/TemplateB/MedicalAndPatient/setting1.html";
+}
+function switchToPPerformanceReview() {
+	window.location.href = "../../../Personal/TemplateB/PerformanceReview/setting1.html";
+}
+function switchToPFamilyReunion() {
+	window.location.href = "../../../Personal/TemplateC/FamilyReunion/setting1.html";
+}
+function switchToPMeetingCoordination() {
+	window.location.href = "../../../Personal/TemplateC/MeetingCoordination/setting1.html";
+}
+function switchToPSocialEvent() {
+	window.location.href = "../../../Personal/TemplateC/SocialEvent/setting1.html";
+}
+function switchToPGroupStudySession() {
+	window.location.href = "../../../Personal/TemplateC/GroupStudySession/setting1.html";
+}
+function switchToPVacationPlanning() {
+	window.location.href = "../../../Personal/TemplateC/VacationPlanning/setting1.html";
+}
+function switchToPTripPlanning() {
+	window.location.href = "../../../Personal/TemplateC/TripPlanning/setting1.html";
+}
+function switchToPRealEstate() {
+	window.location.href = "../../../Personal/TemplateC/RealEstate/setting1.html";
+}
+function switchToPTeamBuildingEvent() {
+	window.location.href = "../../../Personal/TemplateC/TeamBuildingEvent/setting1.html";
+}
+function switchToPAcademicAdvisors() {
+	window.location.href = "../../../Personal/TemplateC/AcademicAdvisors/setting1.html";
+}
+function switchToPGroupProjectMeeting() {
+	window.location.href = "../../../Personal/TemplateC/GroupProjectMeeting/setting1.html";
+}
+
+
+// Small Business
+
+function switchToSBClientsMeeting() {
+	window.location.href = "../../../SmallBusiness/TemplateB/ClientsMeeting/setting1.html";
+}
+function switchToSBConference() {
+	window.location.href = "../../../SmallBusiness/TemplateB/ClientsMeeting/setting1.html";
+}
+function switchToSBGovernmentAgency() {
+	window.location.href = "../../../SmallBusiness/TemplateA/GovernmentAgency/setting1.html";
+}
+function switchToSBInterviews() {
+	window.location.href = "../../../SmallBusiness/TemplateB/Interviews/setting1.html";
+}
+function switchToSBMedicalAndPatient() {
+	window.location.href = "../../../SmallBusiness/TemplateB/MedicalAndPatient/setting1.html";
+}
+function switchToSBMeetingCoordination() {
+	window.location.href = "../../../SmallBusiness/TemplateC/MeetingCoordination/setting1.html";
+}
+function switchToSBPerformanceReview() {
+	window.location.href = "../../../SmallBusiness/TemplateB/PerformanceReview/setting1.html";
+}
+function switchToSBStoreAppointment() {
+	window.location.href = "../../../SmallBusiness/TemplateA/BabyShower/setting1.html";
+}
+function switchToSBCompanyOuting() {
+	window.location.href = "../../../SmallBusiness/TemplateA/Babysitting/setting1.html";
+}
+function switchToSBPersonalTrainer() {
+	window.location.href = "../../../SmallBusiness/TemplateA/SportGames/setting1.html";
+}
+function switchToSBRealEstate() {
+	window.location.href = "../../../SmallBusiness/TemplateA/Wedding/setting1.html";
+}
+function switchToSBTripPlanning() {
+	window.location.href = "../../../SmallBusiness/TemplateC/FamilyReunion/setting1.html";
+}
+function switchToSBTutoringScheduling() {
+	window.location.href = "../../../SmallBusiness/TemplateC/SocialEvent/setting1.html";
+}
+
+
+// Domain
+
+function switchToDClientsMeeting() {
+	window.location.href = "../../../Domain/TemplateB/ClientsMeeting/setting1.html";
+}
+function switchToDConference() {
+	window.location.href = "../../../Domain/TemplateB/ClientsMeeting/setting1.html";
+}
+function switchToDGovernmentAgency() {
+	window.location.href = "../../../Domain/TemplateA/GovernmentAgency/setting1.html";
+}
+function switchToDGroupStudySession() {
+	window.location.href = "../../../Personal/TemplateC/GroupStudySession/setting1.html";
+}
+function switchToDGroupProjectMeeting() {
+	window.location.href = "../../../Personal/TemplateC/VacationPlanning/setting1.html";
+}
+function switchToDInterviews() {
+	window.location.href = "../../../Domain/TemplateB/Interviews/setting1.html";
+}
+function switchToDMedicalAndPatient() {
+	window.location.href = "../../../Domain/TemplateB/MedicalAndPatient/setting1.html";
+}
+function switchToDMeetingCoordination() {
+	window.location.href = "../../../Domain/TemplateC/MeetingCoordination/setting1.html";
+}
+function switchToDPTConference() {
+	window.location.href = "../../../Domain/TemplateC/Parent-TeacherConference/setting1.html";
+}
+function switchToDPerformanceReview() {
+	window.location.href = "../../../Domain/TemplateB/PerformanceReview/setting1.html";
+}
+function switchToDSocialEvent() {
+	window.location.href = "../../../Personal/TemplateC/SocialEvent/setting1.html";
+}
+function switchToDSportGames() {
+	window.location.href = "../../../Personal/TemplateA/SportGames/setting1.html";
+}
+function switchToDStoreAppointment() {
+	window.location.href = "../../../Domain/TemplateA/BabyShower/setting1.html";
+}
+function switchToDTeamBuildingEvent() {
+	window.location.href = "../../../Personal/TemplateC/VacationPlanning/setting1.html";
+}
+function switchToDTripPlanning() {
+	window.location.href = "../../../Domain/TemplateC/FamilyReunion/setting1.html";
+}
+function switchToDTutoringScheduling() {
+	window.location.href = "../../../Domain/TemplateC/SocialEvent/setting1.html";
+}
+function switchToDTutoringScheduling() {
+	window.location.href = "../../../Domain/TemplateC/SocialEvent/setting1.html";
+}
+function switchToDVacationPlanning() {
+	window.location.href = "../../../Personal/TemplateC/VacationPlanning/setting1.html";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+// old version =======================================================================================================
+
+
+
 function AppLevelBack() {
 	window.location.href = "../AppListNav.html";
 }
