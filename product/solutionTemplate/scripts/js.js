790a6f7ffaf43565156126c677509ff0282420f0
Jasong-1 <gzjxigongda@gmail.com>
2023-06-30 21:50:41 +0800

    06302023.1 text update

--- product/solutionTemplate/scripts/js.js
+++ product/solutionTemplate/scripts/js.js
@@ -1,29 +1,48 @@
-
+// 获取当前url,根据/分割url
 var currentLocation = location.href.split('/');
 
+// 获取最后一个/后的字符串
+// var pageNameAddValue = currentLocation[currentLocation.length - 1];
 var currentPageName = currentLocation[currentLocation.length - 1];
 
+// 分割文件名和参数
+// var pageNameAddValueSplit =  pageNameAddValue.split('?');
+
+// 获取文件名
+// var currentPageName = pageNameAddValueSplit[pageNameAddValueSplit.length - 2];
+
+// 获取navbutton node list
 var navBtn = document.querySelectorAll("li.nav-item a");
 
 
 for (var i = 0; i < navBtn.length; i++) {
+	
+	var url = navBtn[i].getAttribute('href');
 
-	let url = navBtn[i].getAttribute('href');
-
-	if (url === currentPageName) {
+	if (currentPageName === url) {
 		navBtn[i].classList.add("active");
 	}
-	else if (url === "setting2.html") {
-		navBtn[i].classList.add("active");
+	else if (currentPageName === "setting2.html") {
+		navBtn[0].classList.add("active");
 	}
-	else if (url === 'setting3.html') {
-		navBtn[i].classList.add("active");
+	else if (currentPageName === "setting3.html") {
+		navBtn[0].classList.add("active");
 	}
-	else if (url === 'setting4.html') {
-		navBtn[i].classList.add("active");
+	else if (currentPageName === "setting4.html") {
+		navBtn[0].classList.add("active");
 	}
-	else if (url === 'setting5.html') {
-		navBtn[i].classList.add("active");
+	else if (currentPageName === "setting5.html") {
+		navBtn[0].classList.add("active");
 	}
-	else{}
+	// else{
+	// 	navBtn[0].classList.add("active");
+	// }
 };
+
+
+// 获取当前event文件夹名
+var currentEventName = currentLocation[currentLocation.length - 2];
+// 修改tab标签名
+document.getElementsByTagName("title")[0].innerText = currentEventName;
+
+
